5c08ce6fc761a4e658b3d856909437f9ec151015
sjcexplorer <samjcox@hotmail.com>
2021-05-13 20:13:58 +0100

    Remove unecessary Flash messages
    Where an action is obvious by the results (such as succesful login), a
    flash message is not required as the success is evident by the result.
    These uncessary flash messages have been removed, leaving only the
    instances where the result/error/action is not clear to the user.

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ from functools import wraps
 
 # Non-standard modules
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_caching import Cache
+# from flask_caching import Cache
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-import pylibmc
+# import pylibmc
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -37,64 +37,64 @@ def after_request(response):
 
 # Configure Memcache for session storage, following this Heroku guide:
 # https://devcenter.heroku.com/articles/flask-memcache
-cache = Cache()
-cache_servers = os.environ.get('MEMCACHIER_SERVERS')
-if cache_servers == None:
-    cache.init_app(app, config={'CACHE_TYPE': 'simple'})
-else:
-    cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
-    cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
-    cache.init_app(app,
-        config={'CACHE_TYPE': 'saslmemcached',
-                'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
-                'CACHE_MEMCACHED_USERNAME': cache_user,
-                'CACHE_MEMCACHED_PASSWORD': cache_pass,
-                'CACHE_OPTIONS': { 'behaviors': {
-                    # Faster IO
-                    'tcp_nodelay': True,
-                    # Keep connection alive
-                    'tcp_keepalive': True,
-                    # Timeout for set/get requests
-                    'connect_timeout': 2000, # ms
-                    'send_timeout': 750 * 1000, # us
-                    'receive_timeout': 750 * 1000, # us
-                    '_poll_timeout': 2000, # ms
-                    # Better failover
-                    'ketama': True,
-                    'remove_failed': 1,
-                    'retry_timeout': 2,
-                    'dead_timeout': 30}}})
-    app.config.update(
-        SESSION_TYPE = 'memcached',
-        SESSION_MEMCACHED =
-            pylibmc.Client(cache_servers.split(','), binary=True,
-                            username=cache_user, password=cache_pass,
-                            behaviors={
-                                # Faster IO
-                                'tcp_nodelay': True,
-                                # Keep connection alive
-                                'tcp_keepalive': True,
-                                # Timeout for set/get requests
-                                'connect_timeout': 2000, # ms
-                                'send_timeout': 750 * 1000, # us
-                                'receive_timeout': 750 * 1000, # us
-                                '_poll_timeout': 2000, # ms
-                                # Better failover
-                                'ketama': True,
-                                'remove_failed': 1,
-                                'retry_timeout': 2,
-                                'dead_timeout': 30,
-                            })
-    )
-Session(app)
+# cache = Cache()
+# cache_servers = os.environ.get('MEMCACHIER_SERVERS')
+# if cache_servers == None:
+#     cache.init_app(app, config={'CACHE_TYPE': 'simple'})
+# else:
+#     cache_user = os.environ.get('MEMCACHIER_USERNAME') or ''
+#     cache_pass = os.environ.get('MEMCACHIER_PASSWORD') or ''
+#     cache.init_app(app,
+#         config={'CACHE_TYPE': 'saslmemcached',
+#                 'CACHE_MEMCACHED_SERVERS': cache_servers.split(','),
+#                 'CACHE_MEMCACHED_USERNAME': cache_user,
+#                 'CACHE_MEMCACHED_PASSWORD': cache_pass,
+#                 'CACHE_OPTIONS': { 'behaviors': {
+#                     # Faster IO
+#                     'tcp_nodelay': True,
+#                     # Keep connection alive
+#                     'tcp_keepalive': True,
+#                     # Timeout for set/get requests
+#                     'connect_timeout': 2000, # ms
+#                     'send_timeout': 750 * 1000, # us
+#                     'receive_timeout': 750 * 1000, # us
+#                     '_poll_timeout': 2000, # ms
+#                     # Better failover
+#                     'ketama': True,
+#                     'remove_failed': 1,
+#                     'retry_timeout': 2,
+#                     'dead_timeout': 30}}})
+#     app.config.update(
+#         SESSION_TYPE = 'memcached',
+#         SESSION_MEMCACHED =
+#             pylibmc.Client(cache_servers.split(','), binary=True,
+#                             username=cache_user, password=cache_pass,
+#                             behaviors={
+#                                 # Faster IO
+#                                 'tcp_nodelay': True,
+#                                 # Keep connection alive
+#                                 'tcp_keepalive': True,
+#                                 # Timeout for set/get requests
+#                                 'connect_timeout': 2000, # ms
+#                                 'send_timeout': 750 * 1000, # us
+#                                 'receive_timeout': 750 * 1000, # us
+#                                 '_poll_timeout': 2000, # ms
+#                                 # Better failover
+#                                 'ketama': True,
+#                                 'remove_failed': 1,
+#                                 'retry_timeout': 2,
+#                                 'dead_timeout': 30,
+#                             })
+#     )
+# Session(app)
 
 # TEMPORARY FOR FLASK LOCAL TESTING ONLY.
 # Configure session to use filesystem,
 # (instead of signed cookies), sourced from CS50.
-# app.config["SESSION_FILE_DIR"] = mkdtemp()
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # SQLalchemy has removed support for postgress:// scheme which is used
 # by Heroku Postgres, the below maintains compatibility.
@@ -158,7 +158,6 @@ def login():
         session["current_cube_id"] = 0
         session["username"] = request.form.get("username")
         # Redirect to homepage.
-        flash("Login successful")
         return redirect("/")
 
 
@@ -355,8 +354,6 @@ def check_cube():
         db.execute(SQL, data)
         # Commit & close database connection.
         db_close()
-        # Flash message to confirm successful save.
-        flash("Cube " + str(session["current_cube_id"]) + " has been checked and is correct.")
         print("CHECK_CUBE - Completed with no errors.")
         return redirect("/solve")
 
@@ -501,8 +498,6 @@ def load():
     session["cube"] = list[0]
     # Commit & close database connection.
     db_close()
-    # Flash message to user then proceed to solve page.
-    flash("Cube ID " + cube_to_load + " has been loaded.")
     return check_cube()
 
 
@@ -519,8 +514,6 @@ def amend_from_list():
     session["cube"] = cube_loading[0]
     # Commit & close database connection.
     db_close()
-    # Flash message to user then proceed to amend page.
-    flash("Cube ID " + cube_to_amend + " has been loaded for amendment.")
     return redirect("/amend")
 
 
